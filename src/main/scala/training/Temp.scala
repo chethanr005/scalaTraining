e8cae0c9265a494de6e800e6ab4150fdffd4c923
Kishor <kishor.k@reactore.com>
2022-01-10 15:43:05 +0530

    Added new Demo.scala file

--- src/main/scala/training/Temp.scala
+++ src/main/scala/training/Temp.scala
@@ -1,9 +1,19 @@
 package training
 
+import java.util.concurrent.atomic.LongAccumulator
+
 /**
   * Created by Kartik on Jan 06, 2022.
   */
 
 class Temp {
+
+
+
+
+
+}
+
+object Temp extends App {
 
 }
