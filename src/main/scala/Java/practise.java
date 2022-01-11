abc9fb5b42ebac468e4ecbd3afc772dff395fb25
Rakesh <rakesh.m@reactore.com>
2022-01-11 18:05:46 +0530

    practise

--- src/main/scala/Java/practise.java
+++ src/main/scala/Java/practise.java
@@ -1,12 +1,10 @@
 package Java;
 
-import netscape.javascript.JSUtil;
-
-import java.util.Comparator;
+import java.util.*;
 import java.util.function.*;
+import java.util.stream.Collectors;
 
 public class practise {
-
 //    public boolean prime(int n)
 //    {
 //        int c=0;
@@ -23,9 +21,23 @@ public class practise {
 //        else
 //            return true;
 //    }
+    public static Boolean iseven(int a)
+  {
+     if(a%2==0)
+        return true;
+     else
+         return false;
+  }
 
+  public void tables(int n)
+  {
+      for(int i=1;i<=10;i++)
+      {
+          System.out.println(n+" * "+i+" = "+n*i);
+      }
+  }
     public static void main(String[] args) {
-          //practise obj=new practise();
+          practise obj=new practise();
           //System.out.println(obj.prime(27));
         String s="hello";
         //Consumer
@@ -76,9 +88,83 @@ public class practise {
         System.out.println(sup.get());
 
         //Method Reference
-        Function <String,String> sm=(s1)->s1.concat(" Hello"); //Supports only Static values.
+        Function <String,String> sm=(s1)->s1.concat(" Hello"); //Function supports only for function with Single argument.
         Function <String,String> smr=String::toLowerCase;
+        BiFunction<String,String,String> bismr=String::concat;
         System.out.println(sm.apply("Hello"));
         System.out.println(smr.apply("Hello"));
+        System.out.println(bismr.apply("Hello"," Concat"));//BiFUnction supports function with 2 arguments.
+
+        System.out.println(iseven(8));//Static Function implementation
+        Predicate<Integer> isev=practise::iseven;//creating our own class method reference.
+        System.out.println(isev.test(11));
+
+        // Consumer<Integer> tab=practise::tables;// Should make the tables function as static.
+        //tab.accept(7);
+        Consumer<Integer> tab1=obj::tables;//Method references by object , then no need to mention tables as static.
+        tab1.accept(8);
+
+        List<String> names=Arrays.asList("xyz","yzx","zxy");
+        System.out.println(names);
+        System.out.println(names.stream());
+        System.out.println(names.parallelStream());
+
+        //List
+        List<Integer> numlist=new ArrayList<>();
+        numlist.add(1);
+        numlist.add(2);
+        numlist.add(3);
+        numlist.add(4);
+        numlist.add(5);
+        System.out.println(numlist);
+        numlist.remove(2);
+        numlist.add(2,5);
+        System.out.println(numlist);
+        System.out.println(numlist.get(2));
+
+
+        //Stream Example
+        List<Integer> check= Arrays.asList(1, 45, 25, 23, 10,1,23,25,25);
+        List<Integer> num=check.stream().
+                filter(i->i%5==0).collect(Collectors.toList());// Filter()
+
+
+        List<Integer> multi=check.stream()
+                        .map(i->i*2).collect(Collectors.toList());// Map()
+
+        List<Integer> unq=check.stream()
+                .distinct().collect(Collectors.toList());//Distinct()
+
+        long count= check.stream().count();//stream Count();
+        long ccount=check.size();// collection size to find count.
+
+        List<Integer> srt=check.stream().
+        sorted().collect(Collectors.toList());// Sorting
+
+        long res=check.stream()
+                        .reduce(0,(a,b)->a+b);// reducer to find sum of list.
+
+        int res1=check.stream()
+                .reduce(1,(a,b)->a*b);// reducer to find sum of list.
+
+        int maxno=check.stream()
+                .reduce(0,(a,b)->{
+                    if(a>b)
+                        return a;
+                    else
+                        return b;
+                });
+
+
+        System.out.println("Numbers : "+check);
+        System.out.println("%5 : "+num);
+        System.out.println("*2 : "+multi);
+        System.out.println("Distinct : "+unq);
+        System.out.println("Count : "+count);
+        System.out.println("Collection Count : "+ccount);
+        System.out.println("Sorted : "+srt);
+        System.out.println("Sum of elements : "+res);
+        System.out.println("Product of elements : "+res1);
+        System.out.println("Maximum of elements : "+maxno);
     }
 }
