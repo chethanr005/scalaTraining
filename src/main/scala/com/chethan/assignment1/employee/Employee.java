b38da2baa773614f3ba3ba762bba66c6a8b86b1c
chethan <chethan.r@reactore.com>
2022-01-28 15:04:21 +0530

    .

---
+++ src/main/scala/com/chethan/assignment1/employee/Employee.java
@@ -0,0 +1,79 @@
+package com.chethan.assignment1.employee;
+
+import com.chethan.assignment1.student.Student;
+
+import java.time.*;
+import java.util.*;
+import java.util.*;
+
+public class Employee {
+
+    private String name;
+    private String department;
+    private double salary;
+    private String gender;
+    private LocalDate joiningDate;
+    private LocalDate dob; // date of birth
+    private String jobLevel; // date of birth
+
+
+    protected Employee (){}
+    protected Employee(String name, String department, double salary, String gender, LocalDate joiningDate, LocalDate dob, String jobLevel) {
+
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+        this.gender = gender;
+        this.joiningDate = joiningDate;
+        this.dob = dob;
+        this.jobLevel = jobLevel;
+
+    }
+
+    //Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public void setJoiningDate(LocalDate joiningDate) {
+        this.joiningDate = joiningDate;
+    }
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+    public void setJobLevel(String jobLevel) {
+        this.jobLevel = jobLevel;
+    }
+
+
+    //Getters
+    public String getName() {
+        return name;
+    }
+    public String getDepartment() {
+        return department;
+    }
+    public double getSalary() {
+        return salary;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public LocalDate getJoiningDate() {
+        return joiningDate;
+    }
+    public LocalDate getDob() {
+        return dob;
+    }
+    public String getJobLevel() {
+        return jobLevel;
+    }
+}
