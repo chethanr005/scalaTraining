627e6d6c243548309cdf3c6f7da535a66919f1cc
Rakesh <rakesh.m@reactore.com>
2022-01-28 15:38:14 +0530

    assignment1

--- src/main/scala/com/rakesh/assignment1/student/Output.java
+++ src/main/scala/com/rakesh/assignment1/student/Output.java
@@ -3,7 +3,7 @@ package com.rakesh.assignment1.student;
 import java.util.List;
 
 public class Output {
-    public static List<Student> ListOfStudents= StudentDataBase.getAllStudents();
+    public static List<Student> listOfStudents= StudentDataBase.getAllStudents();
     public static void main(String[] args) {
 
         MaleAndFemaleContainer result=new MaleAndFemaleContainer();
@@ -12,17 +12,17 @@ public class Output {
         PerformanceContainer pl=new PerformanceContainer();
 
 
-        System.out.println("Male count : "+result.maleCount(ListOfStudents));
+        System.out.println("Male count : "+result.maleCount(listOfStudents));
         System.out.println();
-        System.out.println("Female count : "+result.femaleCount(ListOfStudents));
+        System.out.println("Female count : "+result.femaleCount(listOfStudents));
         System.out.println();
-        System.out.println("List of Students name : "+result.studentNames(ListOfStudents));
+        System.out.println("List of Students name : "+result.studentNames(listOfStudents));
         System.out.println();
-        System.out.println("Students count based on Grade-level : "+glc.gradeLevelCount(ListOfStudents));
+        System.out.println("Students count based on Grade-level : "+glc.gradeLevelCount(listOfStudents));
         System.out.println();
-        System.out.println("Activity Count List : "+ac.activityCount(ListOfStudents));
+        System.out.println("Activity Count List : "+ac.activityCount(listOfStudents));
         System.out.println();
-        System.out.println("Performance List Count : "+pl.performanceList(ListOfStudents));
+        System.out.println("Performance List Count : "+pl.performanceList(listOfStudents));
 
 
     }
